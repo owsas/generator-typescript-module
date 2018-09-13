75248ad47e61db49ee6917388ecc3b8d6f99bcfe
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@iMac-de-Juan.local>
2018-09-13 09:01:51 -0500

    created clone script

--- sh/clone.sh
+++ sh/clone.sh
@@ -1,5 +1,11 @@
+# Clean the old files
+rm -rf generators/app/typescript-module-template
+
 # Clone the main repo
 git clone https://github.com/owsas/typescript-module-template
+
+# Remove the .git folder
+rm -rf typescript-module-template/.git
 
 # Move the repo to the generators folder
 mv typescript-module-template generators/app
