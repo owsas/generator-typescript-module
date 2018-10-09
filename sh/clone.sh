c33634f2961bef19d4a3d4a50c03854791066426
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@iMac-de-Juan.local>
2018-10-09 17:03:21 -0500

    fixing gitignore problem when cloning

--- sh/clone.sh
+++ sh/clone.sh
@@ -7,5 +7,8 @@ git clone https://github.com/owsas/typescript-module-template
 # Remove the .git folder
 rm -rf typescript-module-template/.git
 
+# Fix gitignore problem
+mv typescript-module-template/.gitignore typescript-module-template/gitignore
+
 # Move the repo to the generators folder
-mv typescript-module-template generators/app
+mv typescript-module-template generators generators/app
